eba052f5b6b3335055c0181f6d9f802073e17cfc
Isaac King <idrkings@gmail.com>
2021-05-20 23:23:24 -0700

    Initial project setup

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>The Coeus Project</title>
+        <meta name="description" content="Questions" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
